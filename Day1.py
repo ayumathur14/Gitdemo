7e006619b89bd521cb8b439f4bf0253f2c051e1d
ayushi <ayushimathur70@gmail.com>
2025-12-31 21:50:45 +0530

    first commit

---
+++ Day1.py
@@ -0,0 +1,24 @@
+
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver=webdriver.Chrome() #webdriver-moddule->Chrome class, driver->object
+
+#driver1=webdriver. Firefox() #it will open firefox browser
+
+#driver2=webdriver. Edge() # #it will open edge browser
+
+driver.get("https://icma-dev.amgen.com/ADDSafety/#/login")
+
+driver.maximize_window()
+
+driver.find_element(By.ID, "UserName").send_keys("amathu01")
+
+#driver2.get("https://icma-dev.amgen.com/ADDSafety/#/login")
+
+print(driver.title)
+
+print(driver.current_url)
+
+time.sleep(2)
