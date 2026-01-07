f03ade385fbfcdfd44810644f338cc8e3cb33ec9
ayushi <ayushimathur70@gmail.com>
2026-01-07 22:00:10 +0530

    updated code at 7jan2026 10:00pm

--- Day1.py
+++ Day1.py
@@ -12,7 +12,7 @@ print("this is day1 code")
 driver.get("https://icma-dev.amgen.com/ADDSafety/#/login")
 
 driver.maximize_window()
-
+print("This is day1 code for python selenium")
 driver.find_element(By.ID, "UserName").send_keys("amathu01")
 
 #driver2.get("https://icma-dev.amgen.com/ADDSafety/#/login")
